3546ddd3ae27e72c46d9c8df19c306263d18bc84
Jan <jangajdosprod@gmail.com>
2024-11-09 16:08:13 +0100

    chore: cleanup project files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['jdxclqrlcbbvsrhxugbz.supabase.co'], // přidejte vaši Supabase doménu
-  },
-  // Přidáme output: 'standalone' pro lepší kompatibilitu s Vercel
-  output: 'standalone'
+    domains: ['jdxclqrlcbbvsrhxugbz.supabase.co'],
+  }
 }
 
 module.exports = nextConfig 
